e7d53cc0ecb597ddb73a9b9d9f49eaf8255559cd
Stefan Doychev <sdoychev@gmail.com>
2019-07-22 17:03:11 +0300

    Participants update

--- HCGJ TeamFormer/Assets/Scripts/HardcoreVariables.cs
+++ HCGJ TeamFormer/Assets/Scripts/HardcoreVariables.cs
@@ -1,31 +1,34 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using UnityEngine;
 
 public class HardcoreVariables : MonoBehaviour {
 
     public static List<string> artists = new List<string>
     {
-        "Georgi Georgiev",
-        "Kiril Pekov",
-        "Mihaela Komitova",
-        "Nikoleta Zhecheva",
-        "Plamen Petroff"
+        "Elena Asenova",
+        "Elena Korucheva",
+        "Georgi Georgiev ♟",
+        "Mihaela Komitova ♟",
+        "Nikoleta Zhecheva ♟",
+        "Plamen Petroff ♟"
     };
 
     public static List<string> designers = new List<string>
     {
-        "Angel Marchev",
-        "Martin Bakardzhiev",
-        "Tsanislav Filipov"
+        "Angel Marchev ♟",
+        "Georgi Bozhilov",
+        "Kiril Pekov ♟",
+        "Martin Doychev ♟",
+        "Stefan Doychev ♟",
+        "Stefan Grigorov",
+        "Tsanislav Filipov ♟"
     };
 
     public static List<string> developers = new List<string>
     {
-        "Anton Slavov",
-        "Boyan Spassov",
-        "Georgi Kostov",
-        "Stefan Doychev"
+        "Boyan Spassov ♟",
+        "Georgi Kostov ♟",
+        "Vasko Georgiev"
     };
 
 }
